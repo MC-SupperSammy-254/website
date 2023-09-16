2f870e114762823d8c1ebad18f6b9b888f4368d6
Jackline-Jebet <jebetjackline2016@gmail.com>
2023-09-16 22:39:32 +0300

    finals

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,20 +5,20 @@ import "../styles/Home.css";
 
 function Home() {
   return (
-    <div className='home'>
-      <div className='backgroundImg' style={{backgroundImage: `url(${BackImage})`}}>
+      <div className='home'>
+          <div className='backgroundImg' style={{ backgroundImage: `url(${BackImage})` }}></div>
+          <div className='headerContainer'>
+              <h1>!MC_Supersammy_254 All The WAy!</h1>
+              <h2>For Legitimate Emceee</h2>
+              <span>&</span>
+              <h3>Event Mastery Services</h3>
+              <Link to="/footer">
+                  <button className='btnHome'>Let's Connect Now</button>
+              </Link>
+          </div>
       </div>
-      <div className='headerContainer'>
-      <h1>!MC_Supersammy_254 All The WAy!</h1>
-      <h2>For Legitimate Emceee</h2>
-        <span>&</span> 
-      <h3>Event Mastery Services</h3>
-      <Link to="/footer">
-      <button className='btnHome'>Let's Connect Now</button>
-      </Link>
-      </div>
-    </div>
   )
 }
+
 
 export default Home
